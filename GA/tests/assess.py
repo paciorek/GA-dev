7790c0fb26e37d27891d0c38d8d55aa495baa3ce
Christopher Paciorek <paciorek@stat.berkeley.edu>
2025-10-31 08:46:35 -0700

    Work on test files.

--- GA/tests/assess.py
+++ GA/tests/assess.py
@@ -1,16 +1,17 @@
+from setup import X_diab, y_diab
 import GA
 import time
 
-## Diabetes
+## Diabetes example
 
-print("Performance on diabetes dataset. This set of best covariates is based on an exhaustive all subsets search using AIC as the fitness score (rather than CV-MSPE), with the MSPE simply the in-sample MSPE (and therefore likely a bit too low) based on that set of predictors. Your predictor set might differ a bit and your CV MSPE might be a bit higher. For this task, if your time reaches into the 10s or 100s of seconds, that would be concerning.")
+print("Performance on diabetes dataset. This set of best covariates is based on an exhaustive all subsets search using AIC as the fitness score (rather than CV-MSPE), with the RMSPE simply the in-sample RMSPE (and therefore likely a bit too low) based on that set of predictors. Your predictor set might differ a bit and your CV RMSPE might be a bit higher. For this task, if your time reaches into the 10s or 100s of seconds, that would be concerning.")
 
-print(f"AIC-based best predictors: ")
-print(f"In-sample RMSPE: ")
+print(f"AIC-based best predictors: TODO")
+print(f"In-sample RMSPE: TODO")
 
 
 t0 = time.time()
-results = GA.select(X, y)
+results = GA.select(X_diab, y_diab)
 fulltime = time.time() - t0
 
 print(f"Selected predictors {results.selected}.")
--- GA/tests/test_base.py
+++ GA/tests/test_base.py
@@ -1,6 +1,7 @@
+from setup import X_diab, y_diab
 import pytest
 import GA
 
 def test_output():
-    assert isinstance(GA(X,y), dict)
+    assert isinstance(GA(X_diab, y_diab), dict)
 
