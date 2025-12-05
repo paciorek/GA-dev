6020deba1212776adaa9e72a84337c5db78ba1f2
Christopher Paciorek <paciorek@stat.berkeley.edu>
2025-12-05 07:55:40 -0800

    Update comment in assess.py.

--- GA/tests/assess.py
+++ GA/tests/assess.py
@@ -4,7 +4,7 @@ import time
 
 ## Diabetes example
 
-print("Performance on diabetes dataset. This set of best covariates is based on an exhaustive all subsets search using AIC as the fitness score (rather than CV-based prediction error), with the R^2 simply the in-sample R^2 (and therefore likely a bit too high) based on that set of predictors. Your predictor set might differ a bit and your CV R^2 might be a bit lower. For this task, if your time reaches into the 10s or 100s of seconds, that would be concerning.\n")
+print("Performance on diabetes dataset. This set of best covariates is based on an exhaustive all subsets search using AIC as the fitness score (rather than CV-based prediction error), with the R^2 simply the in-sample R^2 (and therefore likely a bit too high) based on that set of predictors. Your predictor set might differ a bit and your CV R^2 might be a bit lower. For this task, if your time reaches into the 10s or 100s of seconds (when using a simple method like regression), that would be concerning.\n")
 
 print(f"AIC-based best predictors (0-based indexing): 1,2,3,4,5,8 (sex+bmi+bp+s1+s2+s5)") 
 print(f"In-sample R^2: 0.515")
